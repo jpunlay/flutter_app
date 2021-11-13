1cc216af27d0ac59bba53403ef8169c691a911e3
Jesus Punlay <30503097+jpunlay@users.noreply.github.com>
2021-11-12 21:39:38 -0500

    Add apple login

--- lib/widgets/login.dart
+++ lib/widgets/login.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'dart:developer' as developer;
 
 class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() => _Login();
 }
@@ -17,10 +19,44 @@ Future<void> _handleSignIn() async {
 class _Login extends State<Login> {
   Widget _buildBody() {
     final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
-        primary: Colors.white70,
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        minimumSize: const Size(275, 20),
+        primary: Colors.white,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         textStyle: const TextStyle(fontSize: 20));
 
+    ElevatedButton googleLoginButton = ElevatedButton(
+      style: buttonStyle,
+      child: Wrap(
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/icons/google/google.png',
+            height: 30,
+            width: 30,
+          ),
+          const SizedBox(width: 12),
+          const Text('Sign in with Google'),
+        ],
+      ),
+      onPressed: _handleSignIn,
+    );
+
+    ElevatedButton appleLoginButton = ElevatedButton(
+      style: buttonStyle,
+      child: Wrap(
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/icons/apple/apple.png',
+            height: 30,
+            width: 30,
+          ),
+          const SizedBox(width: 12),
+          const Text('Sign in with Apple'),
+        ],
+      ),
+      onPressed: _handleSignIn,
+    );
     // GoogleSignInAccount? user = _currentUser;
     // if (user != null) {
     //   return Column(
@@ -47,28 +83,17 @@ class _Login extends State<Login> {
     //   );
     // } else {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisAlignment: MainAxisAlignment.start,
+      // Wrap(
       children: <Widget>[
         const Padding(
-            padding: EdgeInsets.fromLTRB(20, 240, 20, 20),
+            padding: EdgeInsets.fromLTRB(20, 400, 20, 20),
             child: Text('Sign in', style: TextStyle(fontSize: 22))),
-        ElevatedButton(
-          style: buttonStyle,
-          child: Wrap(
-            crossAxisAlignment: WrapCrossAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/icons/google/google.png',
-                height: 30,
-                width: 30,
-              ),
-              const SizedBox(width: 12),
-              const Text('Sign in with Google'),
-            ],
-          ),
-          onPressed: _handleSignIn,
-        ),
+        googleLoginButton,
+        const SizedBox(height: 20),
+        appleLoginButton
       ],
+      // ),
     );
   }
   // }
