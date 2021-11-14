81d58d176bdfcfdac5735b755506160577c451c2
Jesus Punlay <30503097+jpunlay@users.noreply.github.com>
2021-11-14 10:26:18 -0500

    Add initial google sign in api impl

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
-        primaryIconTheme: IconThemeData(color: Colors.black)
-      ),
+          primarySwatch: Colors.grey,
+          primaryIconTheme: IconThemeData(color: Colors.black)),
       home: Login(),
     );
   }
 }
-
--- lib/widgets/login.dart
+++ lib/widgets/login.dart
@@ -1,5 +1,15 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'dart:developer' as developer;
+import 'package:google_sign_in/google_sign_in.dart';
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  clientId:
+      '261501057690-qb1gcm5vo7khsikle5oeov9bro89rrh0.apps.googleusercontent.com',
+  scopes: <String>[
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ],
+);
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -10,19 +20,40 @@ class Login extends StatefulWidget {
 
 Future<void> _handleSignIn() async {
   try {
-    developer.log('log me', name: 'my.app.category');
+    await _googleSignIn.signIn();
   } catch (error) {
     print(error);
   }
 }
 
 class _Login extends State<Login> {
+  GoogleSignInAccount? _currentUser;
+  String _contactText = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+      setState(() {
+        _currentUser = account;
+        print(_currentUser);
+      });
+      if (_currentUser != null) {
+        // _handleGetContact(_currentUser!);
+      }
+    });
+    _googleSignIn.signInSilently();
+  }
+
   Widget _buildBody() {
     final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
         minimumSize: const Size(275, 20),
         primary: Colors.white,
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        textStyle: const TextStyle(fontSize: 20));
+        textStyle: const TextStyle(fontSize: 20),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ));
 
     ElevatedButton googleLoginButton = ElevatedButton(
       style: buttonStyle,
@@ -57,58 +88,53 @@ class _Login extends State<Login> {
       ),
       onPressed: _handleSignIn,
     );
-    // GoogleSignInAccount? user = _currentUser;
-    // if (user != null) {
-    //   return Column(
-    //     mainAxisAlignment: MainAxisAlignment.spaceAround,
-    //     children: <Widget>[
-    //       ListTile(
-    //         leading: GoogleUserCircleAvatar(
-    //           identity: user,
-    //         ),
-    //         title: Text(user.displayName ?? ''),
-    //         subtitle: Text(user.email),
-    //       ),
-    //       const Text("Signed in successfully."),
-    //       Text(_contactText),
-    //       ElevatedButton(
-    //         child: const Text('SIGN OUT'),
-    //         onPressed: _handleSignOut,
-    //       ),
-    //       ElevatedButton(
-    //         child: const Text('REFRESH'),
-    //         onPressed: () => _handleGetContact(user),
-    //       ),
-    //     ],
-    //   );
-    // } else {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      // Wrap(
-      children: <Widget>[
-        const Padding(
-            padding: EdgeInsets.fromLTRB(20, 400, 20, 20),
-            child: Text('Sign in', style: TextStyle(fontSize: 22))),
-        googleLoginButton,
-        const SizedBox(height: 20),
-        appleLoginButton
-      ],
-      // ),
-    );
+
+    GoogleSignInAccount? user = _currentUser;
+    if (user != null) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          ListTile(
+            leading: GoogleUserCircleAvatar(
+              identity: user,
+            ),
+            title: Text(user.displayName ?? ''),
+            subtitle: Text(user.email),
+          ),
+          const Text("Signed in successfully."),
+          Text(_contactText),
+          // ElevatedButton(
+          //   child: const Text('SIGN OUT'),
+          //   onPressed: _handleSignOut,
+          // ),
+          // ElevatedButton(
+          //   child: const Text('REFRESH'),
+          //   onPressed: () => _handleGetContact(user),
+          // ),
+        ],
+      );
+    } else {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          const Padding(
+              padding: EdgeInsets.fromLTRB(20, 400, 20, 20),
+              child: Text('Sign in', style: TextStyle(fontSize: 22))),
+          googleLoginButton,
+          const SizedBox(height: 20),
+          appleLoginButton
+        ],
+      );
+    }
   }
-  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-        // appBar: AppBar(
-        //   title: const Text('Google Sign In'),
-        //   backgroundColor: Colors.white,
-        // ),
-        body: ConstrainedBox(
-      constraints: const BoxConstraints.expand(),
-      child: _buildBody(),
-    ));
+      body: ConstrainedBox(
+        constraints: const BoxConstraints.expand(),
+        child: _buildBody(),
+      ),
+    );
   }
 }
